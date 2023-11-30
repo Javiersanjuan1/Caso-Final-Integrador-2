8361d13a555f290c53b68ca5877724fe71e4270f
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-30 12:03:12 +0100

    Initial commit

--- Carga de scripts en tiny-lisp.cpp
+++ Carga de scripts en tiny-lisp.cpp
@@ -28,7 +28,7 @@ void load_script() {
 }
 
 int main() {
-    // Uso de load_script() sin parámetros para ingresar el nombre del arch
+    // Uso de load_script() sin parámetros para ingresar el nombre del arc
     load_script();
 
     // Uso de load_script() con parámetros para mostrar el contenido de
